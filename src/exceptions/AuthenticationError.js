450db96125fe4dcce3e7c5f85dee1cbae75b8e2c
Farhan <farhanafriza24@gmail.com>
2022-09-17 19:54:05 +0700

    done until create jwt

---
+++ src/exceptions/AuthenticationError.js
@@ -0,0 +1,12 @@
+/* eslint-disable require-jsdoc */
+
+const ClientError = require('./ClientError');
+
+class AuthenticationError extends ClientError {
+  constructor(message) {
+    super(message, 401);
+    this.name = 'AuthenticationError';
+  }
+}
+
+module.exports = AuthenticationError;
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -55,6 +55,28 @@ class UsersService {
 
     return result.rows[0];
   }
+
+  async verifyUserCredential(username, password) {
+    const query = {
+      text: 'SELECT id, password FROM users WHERE username = $1',
+      values: [username],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    const { id, password: hashedPassword } = result.rows[0];
+
+    const match = await bcrypt.compare(password, hashedPassword);
+
+    if (!match) {
+      throw new AuthenticationError('Kredensial yang Anda berikan salah');
+    }
+
+    return id;
+  }
 }
 
 module.exports = UsersService;
